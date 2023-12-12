3f261d78ac86d38aaa90c0b7eb68217e047fbc61
Ella J <e245547@edu.sbl.ch>
2023-12-12 16:00:35 +0100

    I attempted to complete exercises 3-4

--- script.js
+++ script.js
@@ -16,24 +16,31 @@ export function exercise02(args) {
 
 export function exercise03(args) {
   const input = args
-  const result = []
+  let count = 0
   for (let i = 0; i < input.length; i++) {
     const currentElement = input[i]
-    if (currentElement === "e")
-      inputElement.textContent = parseInt(inputElement.textContent) + 1
-    result.push(inputElement)
+
+    if (currentElement === "e") {
+      count = count + 1
+    } else if (currentElement === "E") {
+      count = count + 1
+    }
   }
-  
+  return count
 }
 
-export function exercise03(args) 
-const input = args
-  const result = []
+export function exercise04(args) {
+  const input = args
+  let count = 0
   for (let i = 0; i < input.length; i++) {
     const currentElement = input[i]
-    if (currentElement === "E")
-      inputElement.textContent = parseInt(inputElement.textContent) + 1
-    result.push(inputElemnt)
-    return.push (inputElement)
 
+    if (currentElement === "") {
+      count = count + 1
+    }
   }
+  result.push(currentElement)
+}
+if (currentElement.charCode()) {
+  return count
+}
